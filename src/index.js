341898dc10436f9499ce17137f4a02fe4e334702
Eason Huang <yisheng.huang1228@gmail.com>
2023-06-24 18:11:54 +0800

    最基础的jsx Element Object to DOM

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const element = <h1 title="foo">Hello</h1>;
+
+console.log('element', element);
+
+const node  = document.createElement(element.type);
+node['title'] = element.props.title;
+
+const text = document.createTextNode('');
+text['nodeValue'] = element.props.children;
+
+node.appendChild(text);
+
+const container = document.getElementById('root');
+container.appendChild(node);
+
+// React.render(element, container);
