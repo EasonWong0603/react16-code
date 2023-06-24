62d81a584da0e457c3e86d7b0dcaeecf4ad106aa
Eason Huang <yisheng.huang1228@gmail.com>
2023-06-24 18:11:54 +0800

    最基础的jsx Element Object to DOM

---
+++ docs/demo.js
@@ -0,0 +1,15 @@
+/**
+ * type 标签名称
+ * props 属性
+ * title 标签属性
+ * children 子元素
+ */
+
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello',
+  },
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // babel-loader is used to load jsx and es6 code
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]], // presets to load
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React',
+      template: path.join(__dirname, 'src/index.html'), // 源模版文件
+    }),
+  ],
+};
