d35c92b2f4cff5f1abd6451cc43be4719f6d28a2
Eason Huang <yisheng.huang1228@gmail.com>
2023-06-24 18:41:33 +0800

    render递归转异步链式调用

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
-// import React from 'react';
-
 import React from '../react';
 
 const element = (
-  <div>
+  <section>
     <h1 title="foo">
-      Hello&nbsp;
-      <a href="">world</a>
+      <span>Hello</span>
     </h1>
-  </div>
+    <a href="">world</a>
+  </section>
 );
 
-console.log('element', element);
-
-// const node  = document.createElement(element.type);
-// node['title'] = element.props.title;
-
-// const text = document.createTextNode('');
-// text['nodeValue'] = element.props.children;
-
-// node.appendChild(text);
-
 const container = document.getElementById('root');
-// container.appendChild(node);
-
 React.render(element, container);
+
+console.log('element', element);
+console.log('container', container);
