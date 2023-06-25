85d09f29ded6881cfe7e2336e7f14e8d400fb432
Eason Huang <yisheng.huang1228@gmail.com>
2023-06-26 00:21:26 +0800

    兼容函数组件

--- src/index.js
+++ src/index.js
@@ -2,38 +2,10 @@ import React from '../react';
 
 const container = document.getElementById('root');
 
-const updateValue = (e) => {
-  const element = (
-    <div className="father">
-      <input onInput={updateValue} value={e.target.value} />
-      <h2
-        className="new"
-        onInput={(e) => console.log('new')}
-        // onMessage={(e) => console.log('new')}
-      >
-        Hello {e.target.value}
-      </h2>
-      <p>add element</p>
-    </div>
-  );
-  React.render(element, container);
-};
+function App(props) {
+  return <h1>Hello {props.name}</h1>;
+}
 
-const rerender = (value) => {
-  const element = (
-    <div className="father">
-      <p>delete element</p>
-      <input onInput={updateValue} value={value} />
-      <h2
-        style={{ fontSize: '20px' }}
-        // onInput={(e) => console.log('old')}
-        onMessage={(e) => console.log('old')}
-      >
-        Hello {value}
-      </h2>
-    </div>
-  );
-  React.render(element, container);
-};
+const element = <App name="Function"></App>;
 
-rerender('World');
+React.render(element, container);
